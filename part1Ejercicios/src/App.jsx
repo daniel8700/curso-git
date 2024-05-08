de39481e704c5e34c1dbae7b9072c3053b226928
daniel8700 <dany8700albor@gmail.com>
2024-05-07 20:11:35 -0600

    Ejercicio 1.5

--- part1Ejercicios/src/App.jsx
+++ part1Ejercicios/src/App.jsx
@@ -1,37 +1,54 @@
+const Header = ({ title }) => {
+  return <h1>{title}</h1>;
+};
+
+const Part = ({ part, part1}) => (
+  <>
+    <p>
+      {part.title} {part.exercises}
+    </p>
+  </>
+);
+
+const Content = ({ content }) => {
+  return (
+    <div>
+      <Part part={content[0]} />
+      <Part part={content[1]} />
+      <Part part={content[2]} />
+    </div>
+  );
+  };
+
+  const Total = ({ parts }) => {
+    const total = parts[0].exercises+parts[1].exercises+parts[2].exercises;
+    return <p>Number of exercises: {total}</p>;
+  };  
+
 const App = () => {
-  const curso = "Desarrollo de aplicaciones full-stack"
-  const part = [
-    {contenido:"Fundamentos de React",calificacion:10},
-    {contenido:"Usa Props y data",calificacion:7},
-    {contenido:"Estado de un componente",calificacion:14},
-  ]
-  
-  return(
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+
+      {title: 'Fundamentals of React',
+      exercises: 10},
+      {title: 'Using props to pass data',
+      exercises: 7},
+      {title: 'State of a component',
+      exercises: 14}
+
+    ],
+    propiedades: "Prueba de funcionamiento"
+  };
+
+  return (
     <div>
-      <Header titulo={curso}/>
-      <Content contenido = {part[0].contenido}calificacion = {part[0].calificacion}></Content>
-      <Content contenido = {part[1].contenido}calificacion = {part[1].calificacion}></Content>
-      <Content contenido = {part[2].contenido}calificacion = {part[2].calificacion}></Content>
-      <Total total = {part[0].calificacion + part[1].calificacion + part[2].calificacion}></Total>
+      <Header title={course.name} />
+      <Content content={course.parts} />
+      <Total parts={course.parts} />
+      <Header title={course.propiedades}/>
     </div>
-  )
-}
-const Header =(props) => {
-  return(
-    <div>
-      <h1>{props.titulo}</h1>
-    </div>
-  )
-}
-const Content =(props) =>{
-  return(
-    <div><p>{props.contenido} {props.calificacion}</p></div>
-  )
-}
-const Total =(props)=>{
-  return(
-    <div><p> Total de puntaje en los ejercicios {props.total}</p></div>
-  )
-}
+  );
+};
 
 export default App
