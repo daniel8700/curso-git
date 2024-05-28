e41bd52710a3dbe451945a51b315599b60d361ec
daniel8700 <dany8700albor@gmail.com>
2024-05-27 19:18:49 -0600

    Refactorizacion de componentes ejercicio de parte 1 C

--- part1C/src/App.jsx
+++ part1C/src/App.jsx
@@ -2,14 +2,29 @@ import { useState } from "react"
 
 const App = () => {
   const [counter, setCounter] = useState(0)
+  const increaseaByOne = () => setCounter(counter+1)
+  const decreaseaByOne = () => setCounter(counter-1)
+  const setZero = () => setCounter(0)
 
-  setTimeout(
-    () => setCounter(counter+1),1000
-  )
-  console.log("rederizando ...", counter)
   return (
-    <div>{counter}</div>
+    <div>
+      <Display counter = {counter}></Display>
+      <Button onSmash = {increaseaByOne} text = "plus"></Button>
+      <Button onSmash = {setZero} text = 'zero'></Button>
+      <Button onSmash = {decreaseaByOne} text = "minus"></Button>
+    </div>
+
+
   )
 }
 
+const Display = ({counter}) => <div>{counter}</div>
+  
+
+
+const Button = ({onSmash, text}) => {
+  return (
+    <button onClick={onSmash}
+>{text}</button>  )
+}
 export default App
