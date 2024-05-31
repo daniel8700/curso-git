afe60b6a667906f6b4b77db06143890dff1cff32
daniel8700 <dany8700albor@gmail.com>
2024-05-30 20:35:02 -0600

    parte 1 d consola

--- part1C/src/App.jsx
+++ part1C/src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   const [right, setright] = useState(0)
   const [allClicks, setAll] = useState([])
   const [total, setTotal] = useState(0)
-
+  
   const handleRightClick = () => {
     setAll(allClicks.concat('R'))
     const updateR = right+1
@@ -20,13 +20,42 @@ const App = () => {
     setTotal(updateL+right)
   }
 
+  const clearClick = () => {
+    setTotal(0)
+    setLeft(0)
+    setright(0)
+    setAll([])
+  }
+
+  const History = (props) => {
+    console.log(props)
+    if(props.allClicks.length === 0){
+      return(
+        <div>
+          La aplicacion se utiliza presionando los botones
+        </div>
+      )
+    }
+    return(
+    <div>
+      Historial de botones presionados: {props.allClicks.join(" ")}
+    </div>
+    )
+  }
+
+  const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
   return (
     <div>
       {left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRightClick}>rigth</button>
+      <Button handleClick={handleLeftClick} text='left' />
+      <button onClick={clearClick}>clear</button>      
+      <Button handleClick={handleRightClick} text='right' />
       {right}
-      <p>{allClicks.join(',')}</p>
+      <History allClicks={allClicks}></History>
       <p>total {total}</p>
     </div>
   )
