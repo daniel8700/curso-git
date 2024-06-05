88fcd90486936945b3729686ee63e283c9bd44d7
daniel8700 <dany8700albor@gmail.com>
2024-06-04 20:59:32 -0600

    parte 1 funcion que devuelve una funcionn

--- part1C/src/App.jsx
+++ part1C/src/App.jsx
@@ -1,63 +1,28 @@
 import { useState } from "react"
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setright] = useState(0)
-  const [allClicks, setAll] = useState([])
-  const [total, setTotal] = useState(0)
-  
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    const updateR = right+1
-    setright(updateR)
-    setTotal(left+updateR)
+  const [value, setValue] = useState(10)
+
+  const handleCick = () => {
+    console.log("click en el boton")
+    setValue(0)
+  }
+  const hello = (who) => () => {
+    console.log("hello world", who)
+  }
+  const setToValue = (newValue) => () => {
+    console.log("el nuevo valor es", newValue)
+    setValue(newValue)
   }
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    const updateL = left+1
-    setLeft(updateL)
-    setTotal(updateL+right)
-  }
-
-  const clearClick = () => {
-    setTotal(0)
-    setLeft(0)
-    setright(0)
-    setAll([])
-  }
-
-  const History = (props) => {
-    console.log(props)
-    if(props.allClicks.length === 0){
-      return(
-        <div>
-          La aplicacion se utiliza presionando los botones
-        </div>
-      )
-    }
-    return(
-    <div>
-      Historial de botones presionados: {props.allClicks.join(" ")}
-    </div>
-    )
-  }
-
-  const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
+return (
+  <div>
+    {value}
+    <button onClick={hello()}>reset to zero</button>
+    <button onClick={setToValue(value+1)}>world</button>
+    <button onClick={setToValue(10)}>react</button>
+    <button onClick={setToValue(20)}>function</button>
+  </div>
 )
-  return (
-    <div>
-      {left}
-      <Button handleClick={handleLeftClick} text='left' />
-      <button onClick={clearClick}>clear</button>      
-      <Button handleClick={handleRightClick} text='right' />
-      {right}
-      <History allClicks={allClicks}></History>
-      <p>total {total}</p>
-    </div>
-  )
 }
 export default App
