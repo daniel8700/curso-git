99e93625fd67bb95ca689e8e3d92ba79219a0314
daniel8700 <dany8700albor@gmail.com>
2024-06-06 19:08:35 -0600

    parte 1 antes de los ejercicios

--- part1C/src/App.jsx
+++ part1C/src/App.jsx
@@ -1,28 +1,36 @@
 import { useState } from "react"
+const Button = (props) => (
+  <button onClick={props.handleCick}>{props.text}</button>
+)
+const Display = (props) => (
+  <div>{props.value}</div>
+)
+const handleCick = () => {
+  console.log("click en el boton")
+  setValue(0)
+}
+const hello = (who) => () => {
+  console.log("hello world", who)
+  setValue(0)
+}
+
 
 const App = () => {
   const [value, setValue] = useState(10)
 
-  const handleCick = () => {
-    console.log("click en el boton")
-    setValue(0)
-  }
-  const hello = (who) => () => {
-    console.log("hello world", who)
-  }
-  const setToValue = (newValue) => () => {
+  const setToValue = (newValue) => {
     console.log("el nuevo valor es", newValue)
     setValue(newValue)
   }
 
-return (
-  <div>
-    {value}
-    <button onClick={hello()}>reset to zero</button>
-    <button onClick={setToValue(value+1)}>world</button>
-    <button onClick={setToValue(10)}>react</button>
-    <button onClick={setToValue(20)}>function</button>
-  </div>
-)
+
+  return (
+    <div>
+      <Display value={value}></Display>
+      <Button handleCick={() => setToValue(0)} text="clear"></Button>
+      <Button handleCick={() => setToValue(1000)} text="mil"></Button>
+      <Button handleCick={() => setToValue(value + 1)} text="+ 1"></Button>
+    </div>
+  )
 }
 export default App
