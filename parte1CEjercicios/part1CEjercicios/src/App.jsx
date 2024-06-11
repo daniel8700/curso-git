1e77c159561cbd3e498696e010ec07f16319f8d8
daniel8700 <dany8700albor@gmail.com>
2024-06-10 18:53:12 -0600

    parte 1 ejercicios comentarios cerrado

--- parte1CEjercicios/part1CEjercicios/src/App.jsx
+++ parte1CEjercicios/part1CEjercicios/src/App.jsx
@@ -6,15 +6,24 @@ const App = () => {
   
   return(
     <div>
-      <h1><strong>Comentarios</strong></h1>
+      <Negritas text = {"Comentarios"}></Negritas>
       <Button valor={() => setState({...state, bueno: state.bueno + 1})} text="Bueno"></Button>
       <Button valor={() => setState({...state, neutral: state.neutral + 1})} text="Neutral"></Button>
       <Button valor={() => setState({...state, malo: state.malo + 1})} text="Malo"></Button>
-      <p>{state.bueno} {state.neutral} {state.malo}</p>
+      <Negritas text = {"Estadisticas"}></Negritas>
+      <Estadisticas name = {"bueno"}text = {state.bueno}></Estadisticas>
+      <Estadisticas name = {"neutral"}text = {state.neutral}></Estadisticas>
+      <Estadisticas name = {"malo"} text = {state.malo}></Estadisticas>
     </div>
   )
 }
 const Button = (props) => (
   <button onClick={props.valor}>{props.text}</button>
+)
+const Negritas = (props) => (
+  <h1><strong >{props.text}</strong></h1>
+)
+const Estadisticas = (props) => (
+  <p>{props.name} {props.text}</p>
 )
 export default App
