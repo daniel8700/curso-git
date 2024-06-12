bcbc8b3d5fb2ed3dc706cf783713779d36a1a7ac
daniel8700 <dany8700albor@gmail.com>
2024-06-11 21:06:17 -0600

    parte 1 ejercicios comentarios cerrado 1.9

--- parte1CEjercicios/part1CEjercicios/src/App.jsx
+++ parte1CEjercicios/part1CEjercicios/src/App.jsx
@@ -2,18 +2,26 @@ import { useState } from 'react'
 const App = () => {
   const [state, setState] = useState({
     bueno:0, malo:0, neutral:0})
-    console.log(state.bueno)
-  
-  return(
+    const total = state.bueno + state.malo + state.neutral;
+    return(
     <div>
       <Negritas text = {"Comentarios"}></Negritas>
       <Button valor={() => setState({...state, bueno: state.bueno + 1})} text="Bueno"></Button>
       <Button valor={() => setState({...state, neutral: state.neutral + 1})} text="Neutral"></Button>
       <Button valor={() => setState({...state, malo: state.malo + 1})} text="Malo"></Button>
       <Negritas text = {"Estadisticas"}></Negritas>
+      {total > 0 &&(
+        <>
       <Estadisticas name = {"bueno"}text = {state.bueno}></Estadisticas>
       <Estadisticas name = {"neutral"}text = {state.neutral}></Estadisticas>
       <Estadisticas name = {"malo"} text = {state.malo}></Estadisticas>
+      <Estadisticas name = {"Todos"} text = {total}></Estadisticas>
+      <Estadisticas name = {"promedio"} text = {total !==0 ?(state.bueno - state.malo) / total : 0 }></Estadisticas>
+      <Estadisticas name = {"positivo"} text = {total !==0 ?(state.bueno * 100) / total : 0}></Estadisticas>
+      </>)}
+      {total == 0 && (<>
+      <Estadisticas name = {"No se han recibido comentarios"} text = {0}></Estadisticas>
+    </>)}
     </div>
   )
 }
